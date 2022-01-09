30404ce64ec790d18396034e735c3dff9d358b13
Josh Booth <boothjmail@gmail.com>
2022-01-09 18:22:48 -0500

    changed way "in time range" files are found

--- cameras/split_motion.py
+++ cameras/split_motion.py
@@ -6,17 +6,9 @@ import datetime
 import subprocess
 import tempfile
 import logging
-
+import shutil
 
 def filename2numbers(filename):
-    """converts input filename from camera to numbers
-
-    Args:
-        filename ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
     return int(''.join(os.path.splitext(os.path.basename(filename))[0].split('_')[2:]))
 
 def filename2datetime(filename):
@@ -24,14 +16,6 @@ def filename2datetime(filename):
     return datetime.datetime(int(numbers[0:4]), int(numbers[4:6]), int(numbers[6:8]), int(numbers[8:10]), int(numbers[10:12]), int(numbers[12:14]))
 
 def to_datetime(time):
-    """[summary]
-
-    Args:
-        time ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
     time = str(time)
     return datetime.datetime(int(time[0:4]), int(time[4:6]), int(time[6:8]), int(time[8:10]), int(time[10:12]), int(time[12:14]))
 
@@ -62,6 +46,10 @@ def get_hostname(hostname_file):
         return "hostnameError"
     return data
 
+def get_video_length(input_video):
+    result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', input_video], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return float(result.stdout)
+
 ap = argparse.ArgumentParser()
 ap.add_argument("motion_file")
 ap.add_argument("--dry_run")
@@ -70,6 +58,7 @@ args = ap.parse_args()
 home_dir = os.path.split(args.motion_file)[0]
 media_dir = os.path.join(home_dir, 'media/')
 motion_dir = os.path.join(home_dir, 'motion/')
+motion_log_dir = os.path.join(home_dir, 'motion_logs/')
 processing_dir = os.path.join(media_dir, '.processing/')
 logname = 'python_motion.log'
 
@@ -85,25 +74,34 @@ logging.basicConfig(
 )
 
 try:
-    make_dirs([media_dir, motion_dir])
+    make_dirs([media_dir, motion_dir, motion_log_dir])
 except:
     logging.critical("Directories unable to be made")
 
+# move log file to somewhere it can be manipulated
+motion_log_file = os.path.join(motion_log_dir, datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '_motionLog.txt')
+if not args.dry_run:
+    shutil.move(args.motion_file, motion_log_file)
+else:
+    motion_log_file = args.motion_file
+
 try:
-    fp = open(args.motion_file, 'r+')
+    fp = open(motion_log_file, 'r+')
     data = [x for x in fp.read().replace('\x00', '').split('\n') if x != '']
 except:
     logging.error("Motion file not found")
+    sys.exit()
 
 if(len(data) % 2 != 0):
-    logging.warning("Data is an odd number, bad data somewhere in log.")
+    logging.warning("Data length is an odd number, bad data somewhere in log.")
 
 while len(data) > 0: # while there is an even number of lines and there is data
     
     video_files = [os.path.join(media_dir,x) for x in os.listdir(media_dir) if x.endswith('.mp4')]
-    #filename2numbers(video_files[0])
-    motion_times = data[:2]
 
+    motion_times = data[:2] # time length to extract
+
+    # check for abnormal motion logs
     if check_motionlog_entry(motion_times) == 1:
         logging.warning("Double start event detected at 0: {} and 1: {}. Moving to next entry.".format(motion_times[0], motion_times[1]))
         data = data[1:] # fix data then restart loop
@@ -114,28 +112,20 @@ while len(data) > 0: # while there is an even number of lines and there is data
 
     motion_start_time = motionlog2datetime(motion_times[0])
     motion_end_time = motionlog2datetime(motion_times[1])
-    #motion_end_time = int(''.join(re.split('{|}|/|:|\s',motion_times[1])[1:7]))
-    logging.debug("Motion start time: {}, Motion end time: {}".format(motion_start_time, motion_end_time))
+    logging.debug("Data len: {}, Motion start time: {}, Motion end time: {}".format(len(data), motion_start_time, motion_end_time))
 
     # get video files that fall in time range
-
-    # handle no file being file w the correct name
-    video_info = {'motion_videos': [], 'next_video': None}
-    for i in range(len(video_files)-1):
-        if motion_start_time >= filename2datetime(video_files[i]) and motion_start_time < filename2datetime(video_files[i+1]) :
+    video_info = {'motion_videos': [], 'start_time': None, 'stop_time': None}
+    for i in range(len(video_files)):
+        if motion_start_time >= filename2datetime(video_files[i]) and motion_end_time < filename2datetime(video_files[i]) + datetime.timedelta(seconds=get_video_length(video_files[i])):
             video_info['motion_videos'].append(video_files[i])
-            i+=1
-            while(filename2datetime(video_files[i]) <= motion_end_time and i < range(len(video_files)-1)):
-                video_info['motion_videos'].append(video_files[i])
-            video_info['next_video'] = video_files[i]
-            break
-        
-    if len(video_info['motion_videos'] == 0):
+
+    if len(video_info['motion_videos']) == 0:
         logging.critical("No videos found in the time range. Skipping entry.")
         data = data[2:]
         continue
 
-    logging.debug("Videos found for time range: {}".format(video_info))
+    logging.debug("Videos found for time range: {}".format(video_info['motion_videos']))
 
     # combine video files in motion occurs in multiple clips
     if len(video_info['motion_videos']) > 1:
@@ -161,15 +151,15 @@ while len(data) > 0: # while there is an even number of lines and there is data
         video_info['motion_videos'] = [os.path.join(processing_dir, video_info['motion_videos'][0])]
         logging.debug("After concatenation, vidoes in time range: {}".format(video_info))
 
-    video_start_time = filename2datetime(video_info['motion_videos'][0])
-    video_end_time = filename2datetime(video_info['next_video'])
+    video_info['start_time'] = filename2datetime(video_info['motion_videos'][0])
+    video_info['stop_time'] = filename2datetime(video_info['motion_videos'][-1]) + datetime.timedelta(seconds=get_video_length(video_files[i]))
 
-    if(video_end_time <= motion_end_time):
-        logging.debug("End time too close to end of video. video_end_time: {}, motion_end_time: {}".format(video_end_time, motion_end_time))
-        motion_end_time = video_end_time - datetime.teimdelta(seconds=2) # subtract 2 seconds as a buffer
+    if(video_info['stop_time'] <= motion_end_time):
+        logging.debug("End time too close to end of video. video_info['stop_time']: {}, motion_end_time: {}".format(video_info['stop_time'], motion_end_time))
+        motion_end_time = video_info['stop_time'] - datetime.timedelta(seconds=2) # subtract 2 seconds as a buffer
 
     motion_start_from_epoch = seconds_from_epoch(motion_start_time)
-    video_start_from_epoch = seconds_from_epoch(video_start_time)
+    video_start_from_epoch = seconds_from_epoch(video_info['start_time'])
     elapsed_time = seconds_from_epoch(motion_end_time) - motion_start_from_epoch
 
     if not motion_start_from_epoch > video_start_from_epoch:
@@ -191,10 +181,10 @@ while len(data) > 0: # while there is an even number of lines and there is data
     # input file, start time, end time, output file, times need to be in HH:MM:SS form
     # formats: HH:MM:SS for start of video
     #ffmpeg -i ORIGINALFILE.mp4 -acodec copy -vcodec copy -ss 00:15:00 -t 00:15:00 OUTFILE-2.mp4
-    logging.debug("Important values: video_start_time: {} video_end_time: {} motion_start_from_epoch: {} video start_from_epoch: {}\
+    logging.debug("Important values: video_info['start_time']: {} video_info['stop_time']: {} motion_start_from_epoch: {} video start_from_epoch: {}\
          elapsed_time: {} start_time_offset: {} end_time_offset: {} start_time_offset_hhmmss: {}, end_time_offset_hhmmss: {}".format(\
-        video_start_time,video_end_time,motion_start_from_epoch,video_start_from_epoch,elapsed_time,start_time_offset,end_time_offset,\
-            start_time_offset_hhmmss,end_time_offset_hhmmss))
+        video_info['start_time'], video_info['stop_time'], motion_start_from_epoch, video_start_from_epoch, elapsed_time, start_time_offset, end_time_offset,\
+            start_time_offset_hhmmss, end_time_offset_hhmmss))
     logging.info("Cutting {} from {} to {} to output file {}".format(video_info['motion_videos'][0], start_time_offset_hhmmss, \
         end_time_offset_hhmmss, output_filename))
 
@@ -221,3 +211,7 @@ while len(data) > 0: # while there is an even number of lines and there is data
     else:
         data = []
 fp.close()
+
+# remove bad files, but keep the log
+for bad_file in [os.path.join(media_dir,x) for x in os.listdir(media_dir) if x.endswith('.bad')]:
+    os.remove(bad_file)
