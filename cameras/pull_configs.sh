7e1f9a7f184b8936eec7f619319ca7bcb125b1ba
Josh Booth <boothjmail@gmail.com>
2022-01-09 23:50:40 +0000

    added camera config files

---
+++ cameras/pull_configs.sh
@@ -0,0 +1,3 @@
+cp /var/www/uconfig config/uconfig
+cp /var/www/schedule.json config/scheudle.json
+cp /etc/motion/motion.conf config/motion.conf
--- cameras/rsync_cmd.sh
+++
@@ -1,1 +0,0 @@
-rsync -a --compress --remove-source-files --progress --partial --partial-dir=.rsync_partial/ /home/user/send/.syncing/ user@172.31.203.75:/mnt/c/Users/user/Desktop/security_system/unevaluated/
